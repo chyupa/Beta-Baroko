0dfba0a5f2006eb3b8f03e904ea856097757c58b
vagrant <vagrant@homestead>
2016-01-16 21:19:04 +0000

    Added sessions cart migration

---
+++ database/migrations/2016_01_16_211622_create_seesions_cart_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeesionsCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions_cart', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('session_id');
+            $table->integer('product_id');
+            $table->float('price', 2);
+            $table->float('quantity', 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sessions_cart');
+    }
+}
