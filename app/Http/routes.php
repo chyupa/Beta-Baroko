a59a90afa887f8f896ec577bc01eb5f5b36dd014
chyupa <razvan.toader@gdm.ro>
2016-02-03 21:09:36 +0200

    changed the conversation route to use front controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -47,7 +47,7 @@ Route::group(['middleware' => ['web']], function () {
 
         Route::get('contact', ['uses' => 'FrontController@getContact', 'as' => 'front.get.contact']);
 
-        Route::get('conversation/{uuid}', ['uses' => 'ContactController@getConversation', 'as' => 'front.get.conversation']);
+        Route::get('conversation/{uuid}', ['uses' => 'FrontController@getConversation', 'as' => 'front.get.conversation']);
 
         // Keep in mind that this needs to be the last route in this group otherwise the other ones will return 404
         Route::get('{url}', ['uses' => 'ProductController@getSingleProduct', 'as' => 'front.get.singleProduct']);
@@ -81,4 +81,6 @@ Route::group([
     Route::get('getSubcategoryProducts/{subcategorySlug}', ['uses' => 'CategoryController@getSubcategoryProducts', 'as' => 'back.get.category']);
 
     Route::post('saveContact', ['uses' => 'ContactController@saveContact', 'as' => 'back.post.saveContact']);
+
+    Route::get('getConversation/{uuid}', ['uses' => 'ContactController@getConversation', 'as' => 'back.get.conversation']);
 });
