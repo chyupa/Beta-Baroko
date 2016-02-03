05c6e130de101a0ea115a520f80b34e0eaf2f0e2
chyupa <razvan.toader@gdm.ro>
2016-02-03 20:44:18 +0200

    created a method for getting conversation by uuid

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Baroko\ContactInfo\Repository\ContactInfoRepository;
 use App\Http\Controllers\Controller;
 
+/**
+ * Class ContactController
+ * @package App\Http\Controllers\Front
+ */
 class ContactController extends Controller
 {
-    public function getConversations() {
-        dd('123123');
+    /**
+     * @var ContactInfoRepository
+     */
+    protected $contactRepo;
+
+    /**
+     * ContactController constructor.
+     * @param ContactInfoRepository $contactInfoRepository
+     */
+    public function __construct(ContactInfoRepository $contactInfoRepository) {
+        $this->contactRepo = $contactInfoRepository;
+    }
+
+    /**
+     * Get conversation by UUID
+     *
+     * @param $uuid
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getConversation($uuid) {
+        $conversation = $this->contactRepo->getConversationByUuid($uuid);
+        if (!$conversation) {
+            return response()->json(['error' => 'Could not find conversation']);
+        }
+
+        return response()->json(['success' => $conversation]);
     }
 }
